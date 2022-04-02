8d757dbe919fe9b834185b67a0c5bf9568fe8cd1
JalzDeLezZ <j4mes.delez@gmail.com>
2022-04-02 12:10:39 -0500

    show server

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,26 +1,60 @@
-const xExpress = require ('express');
-const multer = require('multer');
-const path = require('path');
+const express = require('express')
+const multer = require('multer')
+const path = require('path')
+const fs = require('fs')
 
-const router = xExpress.Router();
+const router = express.Router();
 
-const disktorage = multer.diskStorage({
+const diskstorage = multer.diskStorage({
     destination: path.join(__dirname, '../images'),
     filename: (req, file, cb) => {
-        cb(null, Date.now()+ "monkeywit" +file.originalname);
+        cb(null, Date.now() + '-monkeywit-' + file.originalname)
     }
-});
-const fileUpload = multer({
-    storage: disktorage,
-}).single('image');
+})
 
-router.get('/',(req,res) => {
-    res.send('Hello World');
+const fileUpload = multer({
+    storage: diskstorage
+}).single('image')
+
+router.get('/', (req, res) => {
+    res.send('Welcome to my image app')
 })
 
 router.post('/images/post', fileUpload ,(req,res) => {
-    console.log(req.files);
-    res.send('Hello World');
+
+    req.getConnection((err, conn) => {
+        if(err) return res.status(500).send('server error')
+
+        const type = req.file.mimetype
+        const name = req.file.originalname
+        const data = fs.readFileSync(path.join(__dirname, '../images/' + req.file.filename))
+        
+        conn.query('INSERT INTO image set ?', [{type, name, data}], (err, rows) => {
+            if(err) return res.status(500).send('server error')
+
+            res.send('image saved!')
+        })
+    })
 })
+router.get('/images/get' ,(req,res) => {
+
+    req.getConnection((err, conn) => {
+        if(err) return res.status(500).send('server error')
+
+        conn.query('SELECT * FROM image', (err, rows) => {
+            if(err) return res.status(500).send('server error')
+            rows.map(pI => {
+                fs.writeFileSync(path.join(__dirname, '../dbimages/'+pI.id+'monkeywit.png'), pI.data)
+            })
+
+             const imgDir = fs.readdirSync(path.join(__dirname, '../dbimages'))
+
+                res.send(imgDir)
+
+        })
+    })
+})
+
+
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,15 +1,57 @@
+const express = require('express')
+const mysql = require('mysql2')
+const myconn = require('express-myconnection')
+const path = require('path')
+
+const cors = require('cors')
+
+const app = express()
+
+app.use(myconn(mysql, {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '123456789',
+    database: 'images'
+}))
+app.use(cors())
+
+app.use(express.static(path.join(__dirname, 'dbimages')))
+
+app.use(require('./routes/routes'))
+
+app.listen(9000, () => {
+    console.log('server running on', 'http://localhost:' + 9000)
+})
+
+/* 
 const xExpress = require('express');
+const mysql = require('mysql2');
+const myconn = require('express-myconnection');
+const cors = require('cors');
 
 const app = xExpress();
+
+app.use(myconn(mysql, {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '123456789',
+    database: 'images'
+}));
+app.use(cors());
 
 app.use(require('./routes/routes'));
 
 app.listen(9000, () => {
     console.log('Server is running on port','http://localhost:' +9000);
-})
+}) */
 
 /*
+npm init -y
 npm i express
 npm i nodemon
 npm i mulder
+$ npm i mysql2 express-myconnection
+$ npm i cors
 */
